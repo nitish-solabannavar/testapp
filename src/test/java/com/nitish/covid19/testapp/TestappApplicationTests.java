f7be31108ff8b77d65a4c3be21f1d4dd5b3fdda6
nitish-solabannavar <nitish.solabannavar@gmail.com>
2020-05-19 19:36:55 -0400

    project structure

---
+++ src/test/java/com/nitish/covid19/testapp/TestappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nitish.covid19.testapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
