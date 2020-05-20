141e469c60e4d4d7f7b604e53207e82f344f7f95
nitish-solabannavar <nitish.solabannavar@gmail.com>
2020-05-19 21:25:26 -0400

    updating controller

--- src/main/java/com/nitish/covid19/testapp/controller/PatientController.java
+++ src/main/java/com/nitish/covid19/testapp/controller/PatientController.java
@@ -1,9 +1,51 @@
 package com.nitish.covid19.testapp.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.nitish.covid19.testapp.pojo.Patient;
+import com.nitish.covid19.testapp.service.PatientService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
 
 @RestController
-@RequestMapping("/patient")
+@RequestMapping("/testapp")
 public class PatientController {
+
+    @Autowired
+    PatientService patientService;
+
+    @GetMapping("/")
+    public String hello(){
+        return "<h2>Hello World!</h2>";
+    }
+
+    @GetMapping("/patient")
+    public List<Patient> getPatients(){
+        return patientService.getPatient();
+    }
+
+    @GetMapping("/patient/{patientId}")
+    public Patient getPatientById(@PathVariable int patientId){
+        return patientService.getPatientById(patientId);
+    }
+
+    @PostMapping("/patient")
+    public void savePatient(@Valid @RequestBody Patient patient){
+        try {
+            patientService.createPatient(patient);
+        }catch(Exception ex){
+            ex.printStackTrace();
+        }
+    }
+
+    @PutMapping("/patient")
+    public void updatePatient(@Valid @RequestBody Patient patient){
+        patientService.updatePatient(patient);
+    }
+
+    @DeleteMapping("/patient/{patientId}")
+    public void deletePatient(@PathVariable int patientId){
+        patientService.deletePatient(patientId);
+    }
 }
