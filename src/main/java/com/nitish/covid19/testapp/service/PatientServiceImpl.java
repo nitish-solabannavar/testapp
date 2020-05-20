1854bffd348ccd98dc7ae526a681edfce383d98f
nitish-solabannavar <nitish.solabannavar@gmail.com>
2020-05-19 21:09:58 -0400

    adding controller

--- src/main/java/com/nitish/covid19/testapp/service/PatientServiceImpl.java
+++ src/main/java/com/nitish/covid19/testapp/service/PatientServiceImpl.java
@@ -4,10 +4,12 @@ package com.nitish.covid19.testapp.service;
 import com.nitish.covid19.testapp.pojo.Patient;
 import com.nitish.covid19.testapp.repository.PatientRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
+@Service
 public class PatientServiceImpl implements PatientService {
 
     @Autowired
