2d3b713ff7c7af2c17351ce699af0ebdcf83313e
nitish-solabannavar <nitish.solabannavar@gmail.com>
2020-05-19 20:54:14 -0400

    patient pojo

---
+++ src/main/java/com/nitish/covid19/testapp/pojo/Patient.java
@@ -0,0 +1,90 @@
+package com.nitish.covid19.testapp.pojo;
+
+
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+
+@Entity
+public class Patient {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @NotNull(message = "First Name cannot be null")
+    @NotBlank
+    private String firstName;
+
+    @NotNull(message = "Last Name cannot be null")
+    @NotBlank
+    private String lastName;
+
+    @NotNull(message = "Email cannot be null")
+    @NotBlank
+    @Pattern(regexp = "^[_A-Za-z0-9-+]+(\\.[_A-Za-z0-9-]+)*@[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$",message="Please provide a valid email address")
+    private String email;
+
+    @NotNull(message = "Age cannot be null")
+    @NotBlank
+    private int age;
+
+    @NotNull(message = "Password cannot be null")
+    @Pattern(regexp = "(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])(?=\\S+$).{8,}",message="Password must contain min 8 characters, a uppercase letter, a lowercase letter and a special character")
+    private String password;
+
+    public Patient(){}
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
