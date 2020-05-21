ced91a175a676f390c0a882588732c1d604f4e6a
nitish-solabannavar <nitish.solabannavar@gmail.com>
2020-05-20 21:05:58 -0400

    adding testcontroller

---
+++ src/main/java/com/nitish/covid19/testapp/controller/TestController.java
@@ -0,0 +1,10 @@
+package com.nitish.covid19.testapp.controller;
+
+import com.nitish.covid19.testapp.service.TestService;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    TestService testService;
+}
